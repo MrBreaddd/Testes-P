acca9cb35c7e7fcaf29aa3a289d8c671825a2f1f
MrBreaddd <nathanmorais5@hotmail.com>
2023-10-23 17:06:36 -0300

    Messagens de Commits são Importantes
    Criação de repetição infinita de mensagem com quebra de linha

--- Hello_World.js
+++ Hello_World.js
@@ -1,3 +1,8 @@
-for(let i = 1; i > 100; i++){
-    console.log("Hello World")
+let i2 = 1;
+for(let i = 1; i > 0; i++){
+    console.log("Pão");
+    if(i = i2 + 500){
+        i2 = i;
+        console.log("/n");
+    }
 }
